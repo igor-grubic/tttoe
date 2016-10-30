ed1a1fd51a3db5a2aa74533d0c8c179136185e5c
igor-grubic <igor.grubic@gmail.com>
2016-10-30 17:31:12 +1300

    added a few tests

--- src/game.js
+++ src/game.js
@@ -1,3 +1,37 @@
 export default class Game {
-  
+  constructor() {
+    this.stateEmpty = 0;
+    this.statePlayerOne = 1;
+    this.statePlayerTwo = 2;
+    this.grid = [
+      [this.stateEmpty,this.stateEmpty,this.stateEmpty],
+      [this.stateEmpty,this.stateEmpty,this.stateEmpty],
+      [this.stateEmpty,this.stateEmpty,this.stateEmpty]
+    ]
+  }
+
+  field(pos) {
+    if (!this.isValidField(pos)) return undefined;
+    
+    return this.grid[pos[0]][pos[1]];
+  }
+
+  takeField(pos, player) {
+    if(!this.isValidField(pos) || !this.isValidPlayer(player)) return;
+    if(this.field(pos) != this.stateEmpty) return;
+
+    this.grid[pos[0]][pos[1]] = player;
+  }
+
+  isOver() {
+    return true;
+  }
+
+  isValidField(pos) {
+    return pos[0] >= 0 && pos[0] <= 2 && pos[1] >= 0 && pos[1] <= 2;
+  }
+
+  isValidPlayer(player) {
+    return player === this.statePlayerOne || player === this.statePlayerTwo;
+  }
 }
--- test/game.js
+++ test/game.js
@@ -5,6 +5,11 @@ import Game from '../src/game.js';
 const expect = chai.expect;
 chai.use(dirtyChai);
 
+var getValidGridFields = function () {
+  return [
+    [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]
+  ];
+};
 describe('game', () => {
   let game;
 
@@ -12,7 +17,41 @@ describe('game', () => {
     game = new Game();
   });
 
+  it('has an othodogonal grid of 3 rows and 3 columns', () => {
+    const testValidGridFields = getValidGridFields();
+    const testInvalidGridFields = [
+      [-1,0], [100, 1], [3,2]
+    ];
+
+    testInvalidGridFields.forEach(pos => {
+      expect(game.field(pos)).not.to.exist();
+    });
+    testValidGridFields.forEach(pos => {
+      expect(game.field(pos)).to.equal(0);
+    });
+  });
+
+  it('a player can take a field if not already taken', () => {
+    const pos = [0, 0];
+    game.takeField(pos, game.statePlayerOne);
+
+    expect(game.field(pos)).to.equal(game.statePlayerOne);
+
+    game.takeField(pos, game.statePlayerTwo);
+    expect(game.field(pos)).to.equal(game.statePlayerOne);
+  });
+
+  it('players take turns taking fields until the game is over', () => {
+      expect(false).to.be.true();
+  });
+
   it('a game is over when all fields are taken', () => {
-    expect(true).to.be.true;
+    const validGridFields = getValidGridFields();
+
+    validGridFields.forEach(pos => {
+      game.takeField(pos, game.statePlayerOne);
+    });
+
+    expect(game.isOver()).to.be.true();
   })
 });
